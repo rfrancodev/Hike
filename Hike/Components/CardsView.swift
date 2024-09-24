81fc0d5485b70b59509e2c169d922b922c535783
rfrancodev <francorfs@icloud.com>
2024-09-24 15:01:44 -0300

    Funcion to create random image and animation

--- Hike/Components/CardsView.swift
+++ Hike/Components/CardsView.swift
@@ -8,6 +8,26 @@
 import SwiftUI
 
 struct CardsView: View {
+    
+    //MARK: - PROPETIES
+    @State private var imageNumber: Int = 1
+    @State private var randomNumber: Int = 1
+    
+    //MARK: - FUNCTION
+    //MARK: DEBUG - Testing functionaries of buttom
+    func randomImage() {
+        print("--- BUTTON WAS PRESSED ---")
+        print("Status: Old image number = \(imageNumber)")
+        repeat {
+            randomNumber = Int.random(in: 1...5)
+            print("Action: Random number generated is = \(randomNumber)")
+        } while randomNumber == imageNumber
+            imageNumber = randomNumber
+        print("Result: New image number = \(imageNumber)")
+        print("-- END OF FUNCTION --")
+        print("\n")
+        }
+    
     var body: some View {
         ZStack {
             CustomBackgroundView()
@@ -28,14 +48,14 @@ struct CardsView: View {
                                     ],
                                     startPoint: .top,
                                     endPoint: .bottom
+                                    )
                                 )
-                            )
                         
                         Spacer()
                         
                         Button{
                             // ACTION: Mostrar uma planilha
-                            print("Botao foi pressionado")
+//                            print("Botao foi pressionado")
                         } label: {
                             CustomButtonView()
                         }
@@ -59,17 +79,37 @@ struct CardsView: View {
                                 ],
                                 startPoint: .topLeading,
                                 endPoint: .bottomLeading
+                                )
                             )
-                        )
-                        .frame(width: 256, height: 256)
+                            .frame(width: 256, height: 256)
                     
-                    Image("image-1")
+                    Image("image-\(imageNumber)")
                         .resizable()
                         .scaledToFit()
+                        .animation(.easeOut(duration: 0.4), value: imageNumber)
                 }
 //                MARK: - FOOTER
-            }//:VSTACK
-        }
+                Button{
+                // ACTION: - Pressionar o botao
+                    print("O botao Explorar foi pressionado")
+                    randomImage()
+                } label: {
+                    Text("Explorar Mais")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(
+                            LinearGradient(colors: [
+                                .customGreenLight,
+                                .customGreenMedium],
+                                   startPoint: .top,
+                                   endPoint: .bottom
+                                )
+                            )
+                            .shadow(color: .black.opacity(0.25), radius: 0.25, x:1 , y:2)
+                }
+                .buttonStyle(GradientButtonStyle())
+            }//:ZSTACK
+        }//: CARD
         .frame(width: 320, height: 570)
     }
 }
--- Hike/Utility/GradientButtonStyle.swift
+++ Hike/Utility/GradientButtonStyle.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct GradientButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration
+            .label
+            .padding(.vertical)
+            .padding(.horizontal, 30)
+            .background(
+                // Conditional statement with nil coalescing
+                // Conditional ? A : B
+                configuration.isPressed ?
+                // A: when the user pressed the button
+                LinearGradient(colors: [
+                    .customGrayMedium,
+                    .customGrayLight],
+                               startPoint: .top,
+                               endPoint: .bottom
+                )
+                :
+                // B: when the Button is not pressed
+                LinearGradient(colors: [
+                    .customGrayLight,
+                    .customGrayMedium],
+                               startPoint: .top,
+                               endPoint: .bottom
+                )
+            )
+            .cornerRadius(40)
+        
+    }
+}
