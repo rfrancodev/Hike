dd5409ca286f479ca019cd2d9bcb1c68a82ca280
rfrancodev <francorfs@icloud.com>
2024-09-24 16:48:32 -0300

    Random effect created

---
+++ Hike/Components/MotionAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  MotionAnimationView.swift
+//  Hike
+//
+//  Created by Rafael Franco on 24/09/24.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MotionAnimationView()
+}
